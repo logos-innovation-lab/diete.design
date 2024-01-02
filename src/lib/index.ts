7981ac565387d55b1d6383c5bc632c888bb9a5b8
Vojtech Simetka <vojtech@simetka.cz>
2024-01-02 12:31:06 +0100

    feat: export icons under diete.design/icons

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,15 +1,19 @@
 // Inputs
-import Button from './components/inputs/button.svelte'
+export { default as Button } from './components/inputs/button.svelte'
+export { default as InputText } from './components/inputs/input-text.svelte'
 
 // Layout
-import Container from './components/layout/container.svelte'
+export { default as Container } from './components/layout/container.svelte'
+export { default as Grid } from './components/layout/grid.svelte'
 
 // Data Display
-import Badge from './components/data-display/badge.svelte'
-import Typography from './components/data-display/typography.svelte'
+export { default as Badge } from './components/data-display/badge.svelte'
+export { default as Typography } from './components/data-display/typography.svelte'
+export { default as Dropdown } from './components/inputs/dropdown.svelte'
+export { default as DropdownItem } from './components/inputs/dropdown-item.svelte'
 
-import Theme from './theme.svelte'
-import type { DarkModeOption } from './types'
+export { default as Theme } from './theme.svelte'
+export type { DarkModeOption } from './types'
 
 // Icons
 import AddComment from './icons/add-comment.svelte'
@@ -17,15 +21,4 @@ import Add from './icons/add.svelte'
 import ArrowDownRight from './icons/arrow-down-right.svelte'
 import ArrowRight from './icons/arrow-right.svelte'
 
-export {
-	Badge,
-	Button,
-	Typography,
-	Theme,
-	Container,
-	type DarkModeOption,
-	AddComment,
-	Add,
-	ArrowDownRight,
-	ArrowRight,
-}
+export { AddComment, Add, ArrowDownRight, ArrowRight }
